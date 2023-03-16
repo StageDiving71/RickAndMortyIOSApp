a4b1249070fd306e1717c91b8e1cb75fd5dd794b
StageDiving71 <vova.byrlinov@gmail.com>
2023-03-16 21:59:15 +0300

    Added docstrings and API related files

---
+++ RickAndMorty/APIClient/RMEndpoint.swift
@@ -0,0 +1,8 @@
+//
+//  RMEndpoint.swift
+//  RickAndMorty
+//
+//  Created by Владимир Бурлинов on 16.03.2023.
+//
+
+import Foundation
---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,8 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Владимир Бурлинов on 16.03.2023.
+//
+
+import Foundation
